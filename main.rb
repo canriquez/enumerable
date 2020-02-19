4b114a8025391dc6efe05dd511055563186fcf8d
Carlos Anriquez <canriquez@gmail.com>
2020-02-19 14:44:30 -0300

    adding my_each_with_index. complete with rubocop fixes

--- main.rb
+++ main.rb
@@ -1,9 +1,37 @@
 module Enumerable
+  # My each method definition
+
   def my_each
+    return to_enum :my_each unless block_given?
+
     arr = self
     index = 0
     while index <= arr.length - 1
       yield (arr[index])
+      index += 1
+    end
+    arr
+  end
+
+  # my_each_with_index method definition
+  def my_each_with_index
+    return to_enum :my_each unless block_given?
+    
+    arr = self
+    index = 0
+    while index <= arr.length - 1
+      yield arr[index], index
+      index += 1
+    end
+    arr
+  end
+
+  # my_select method definition
+  def my_select
+    arr = self
+    index = 0
+    while index <= arr.length - 1
+      yield arr[index], index
       index += 1
     end
     arr
@@ -17,3 +45,25 @@ puts " #{[1, 2, 3, 4].my_each { |x| print x, ' -- ' }} "
 puts ''
 print 'each    :'
 puts " #{[1, 2, 3, 4].each { |x| print x, ' -- ' }} "
+puts ''
+print 'my_each no-block: '
+puts [1, 2, 3].my_each
+puts ''
+print 'each no-block   : '
+puts [1, 2, 3].my_each
+puts ''
+
+puts '============== test 1: my_each_with_index =============='
+puts ''
+print 'my_each_with_index :'
+puts " #{[5, 6, 7, 8].my_each_with_index { |x, y| print x * y }} "
+puts ''
+print 'each_with_index    :'
+puts " #{[5, 6, 7, 8].each_with_index { |x, y| print x * y }} "
+puts ''
+print 'my_each_with_index no-block: '
+puts [1, 2, 3].my_each_with_index
+puts ''
+print 'each_with_index no-block   : '
+puts [1, 2, 3].my_each_with_index
+puts ''
