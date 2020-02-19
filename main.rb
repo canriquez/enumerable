73a0df20e82efc49a8960fbdbaa540dfc17d1149
Carlos Anriquez <canriquez@gmail.com>
2020-02-19 13:46:47 -0300

    completed my_each with rubocop adjustments

--- main.rb
+++ main.rb
@@ -1,18 +1,19 @@
 module Enumerable
-    def my_each
-      index = 0
-      while index<=self.length-1
-          yield (self[index])
-          index +=1
-      end
-      return self
+  def my_each
+    arr = self
+    index = 0
+    while index <= arr.length - 1
+      yield (arr[index])
+      index += 1
     end
+    arr
   end
-  
-  puts '============== test 1: my_each =============='
-  puts ''
-  print 'my_each :'
-  p [1, 2, 3, 4].my_each { |x| print x, ' -- '}
-  print 'each    :'
-  p [1, 2, 3, 4].each { |x| print x, ' -- '}
-  
+end
+
+puts '============== test 1: my_each =============='
+puts ''
+print 'my_each :'
+puts " #{[1, 2, 3, 4].my_each { |x| print x, ' -- ' }} "
+puts ''
+print 'each    :'
+puts " #{[1, 2, 3, 4].each { |x| print x, ' -- ' }} "
