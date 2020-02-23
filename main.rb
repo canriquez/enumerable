d764a6c38e782b485aa2625ed3896b4fa7aa10c3
Carlos Anriquez <canriquez@gmail.com>
2020-02-23 08:42:36 -0300

    finished #my_map modified to take procs and blocks. last commit before refactoring to fix rubocop max line limit warning

--- main.rb
+++ main.rb
@@ -131,14 +131,18 @@ module Enumerable
     any_element_true(arr.length, flase_block_elements, false_elements, false_regexp, false_pattern)
   end
 
-  # my_map method definition
-  def my_map
+  # my_map method definition (updated after point 10 of assigment)
+  def my_map(*my_proc)
     caller = self
-    return to_enum :my_map unless block_given?
+    return to_enum :my_map_proc unless block_given? || my_proc[0].class == Proc
 
     back = []
     caller.my_each do |value|
-      back.push(yield value)
+      if my_proc[0].is_a? Proc
+        back.push my_proc[0].call value
+      elsif block_given?
+        back.push(yield value)
+      end
     end
     back
   end
@@ -187,7 +191,7 @@ def multiply_els(arr)
   arr.my_inject(1) { |product, x| x * product }
 end
 
-puts '============== test 1: my_each =============='
+puts '============== Point 1: #my_each =============='
 puts ''
 print 'my_each :'
 puts " #{[1, 2, 3, 4].my_each { |x| print x, ' -- ' }} "
@@ -202,7 +206,7 @@ print 'each no-block   : '
 puts [1, 2, 3].my_each
 puts ''
 
-puts '============== test 1: my_each_with_index =============='
+puts '============== Point 2: #my_each_with_index =============='
 puts ''
 print 'my_each_with_index :'
 puts " #{[5, 6, 7, 8].my_each_with_index { |x, y| print x * y }} "
@@ -217,7 +221,7 @@ print 'each_with_index no-block   : '
 puts [1, 2, 3].my_each_with_index
 puts ''
 
-puts '============== test 1: my_select =============='
+puts '============== Point 3: #my_select =============='
 puts ''
 print 'my_select :'
 puts " #{[5, 6, 7, 8, 10, 11, 12].my_select { |x| (x + 1).even? }} "
@@ -232,91 +236,9 @@ print 'select no-block   : '
 puts [1, 2, 3].select
 puts ''
 
-puts '============== test 1: my_all =============='
-puts ''
-testarr = [2, 10, 99]
-print "#{testarr}.my_all? { |x| x > 1 } :"
-puts " #{testarr.my_all? { |x| x >= 1 }} "
-puts ''
-print "#{testarr}.all? { |x| x > 1 }   :"
-puts " #{testarr.all? { |x| x >= 1 }} "
-puts ''
-print "#{testarr}.my_all? <no-block>: "
-puts testarr.my_all?
-puts ''
-print "#{testarr}.all? <no-block>   : "
-puts testarr.all?
-puts ''
-
-puts '============== test 1: my_any =============='
-puts ''
-testarr = [2, 10, 51]
-print "#{testarr}.my_any? { |x| x > 50 } :"
-puts " #{testarr.my_any? { |x| x > 50 }} "
-puts ''
-print "#{testarr}.any? { |x| x > 50 }   :"
-puts " #{testarr.any? { |x| x > 50 }} "
-puts ''
-print "#{testarr}.my_any? <no-block>: "
-puts testarr.my_any?
-puts ''
-print "#{testarr}.any? <no-block>   : "
-puts testarr.any?
-puts ''
-
-puts '==============  my_none =============='
+puts '==============  Point 4 #my_all =============='
 # rubocop:disable Lint/AmbiguousBlockAssociation
 # rubocop:disable Lint/ParenthesesAsGroupedExpression
-puts 'my_none: Test 1'
-print '%w[ant bear cat].none?    { |word| word.length == 5 } #==>: '
-p %w[ant bear cat].none? { |word| word.length == 5 }
-
-print '%w[ant bear cat].my_none? { |word| word.length == 5 } #==>: '
-p %w[ant bear cat].my_none? { |word| word.length == 5 }
-
-puts 'my_none: Test 2'
-print '%w[ant bear cat].none?    { |word| word.length >= 4 } #==>: '
-p %w[ant bear cat].none? { |word| word.length >= 4 }
-print '%w[ant bear cat].my_none? { |word| word.length >= 4 } #==>: '
-p %w[ant bear cat].my_none? { |word| word.length >= 4 }
-
-puts 'my_none: Test 3'
-print '%w[ant bear cat].none?(/d/)     #==>: '
-p %w[ant bear cat].none?(/d/)
-print '%w[ant bear cat].my_none?(/d/)  #==>: '
-p %w[ant bear cat].my_none?(/d/)
-
-puts 'my_none: Test 4'
-print '[1, 3.14, 42].none?(Float)        #==>: '
-p [1, 3.14, 42].none?(Float)
-print '[1, 3.14, 42].my_none?(Float)     #==>: '
-p [1, 3.14, 42].my_none?(Float)
-
-puts 'my_none: Test 5'
-print '[].none?        #==>: '
-p [].none?
-print '[].my_none?     #==>: '
-p [].my_none?
-
-puts 'my_none: Test 6'
-print '[nil].none?        #==>: '
-p [nil].none?
-print '[nil].my_none?     #==>: '
-p [nil].my_none?
-
-puts 'my_none: Test 7'
-print '[nil,false].none?        #==>: '
-p [nil, false].none?
-print '[nil,false].my_none?     #==>: '
-p [nil, false].my_none?
-
-puts 'my_none: Test 8'
-print '[nil,false,true].none?        #==>: '
-p [nil, false, true].none?
-print '[nil,false,true].my_none?     #==>: '
-p [nil, false, true].my_none?
-
-puts '==============  my_all_full =============='
 
 puts 'my_all_full: Test 1'
 print '%w[ant bear cat].all? { |word| word.length >= 3 }    #==>: '
@@ -324,6 +246,7 @@ p %w[ant bear cat].all? { |word| word.length >= 3 }
 
 print '%w[ant bear cat].my_all? { |word| word.length >= 3 } #==>: '
 p %w[ant bear cat].my_all? { |word| word.length >= 3 }
+puts ''
 
 puts 'my_all_full: Test 2'
 print '%w[ant bear cat].all? { |word| word.length >= 4 }    #==>: '
@@ -331,101 +254,148 @@ p %w[ant bear cat].all? { |word| word.length >= 4 }
 
 print '%w[ant bear cat].my_all? { |word| word.length >= 4 } #==>: '
 p %w[ant bear cat].my_all? { |word| word.length >= 4 }
+puts ''
 
 puts 'my_all_full: Test 3'
 print '%w[ant bear cat].all?(/t/)    #==>: '
 p %w[ant bear cat].all?(/a/)
 print '%w[ant bear cat].my_all?(/t/) #==>: '
 p %w[ant bear cat].my_all?(/a/) { |word| word.length >= 4 }
+puts ''
 
 puts 'my_all_full: Test 4'
 print '[1, "b", 3.14].all?(Numeric)     #==>: '
 p [1, 'b', 3.14].all?(Numeric)
 print '[1, "b", 3.14].my_all?(Numeric)  #==>: '
 p [1, 'b', 3.14].my_all?(Numeric)
+puts ''
 
 puts 'my_all_full: Test 5'
 print '[nil, true, 99].all?        #==>: '
 p [nil, true, 99].all?
 print '[nil, true, 99].my_all?     #==>: '
 p [nil, true, 99].my_all?
+puts ''
 
 puts 'my_all_full: Test 6'
 print '[].all?        #==>: '
 p [].all?
 print '[].my_all?     #==>: '
 p [].my_all?
+puts ''
 
-puts '==============  my_any_full =============='
+puts '==============  Point 5. #my_any =============='
 
 puts 'my_any_full: Test 1'
 print '%w[ant bear cat].any? { |word| word.length >= 3 }    #==>: '
 p %w[ant bear cat].any? { |word| word.length >= 3 }
-
 print '%w[ant bear cat].my_any? { |word| word.length >= 3 } #==>: '
 p %w[ant bear cat].my_any? { |word| word.length >= 3 }
+puts ''
 
 puts 'my_any_full: Test 2'
 print '%w[ant bear cat].any? { |word| word.length >= 4 }    #==>: '
 p %w[ant bear cat].any? { |word| word.length >= 4 }
-
 print '%w[ant bear cat].my_any? { |word| word.length >= 4 } #==>: '
 p %w[ant bear cat].my_any? { |word| word.length >= 4 }
+puts ''
 
 puts 'my_any_full: Test 3'
 print '%w[ant bear cat].any?(/b/)    #==>: '
 p %w[ant bear cat].any?(/b/)
 print '%w[ant bear cat].my_any?(/b/) #==>: '
 p %w[ant bear cat].my_any?(/b/)
+puts ''
 
 puts 'my_any_full: Test 4'
 print '[1, "b", 3.14].any?(Numeric)     #==>: '
 p [1, 'b', 3.14].any?(Numeric)
 print '[1, "b", 3.14].my_any?(Numeric)  #==>: '
 p [1, 'b', 3.14].my_any?(Numeric)
+puts ''
 
 puts 'my_any_full: Test 5'
 print '[nil, true, 99].any?        #==>: '
 p [nil, true, 99].any?
 print '[nil, true, 99].my_any?     #==>: '
 p [nil, true, 99].my_any?
+puts ''
 
 puts 'my_any_full: Test 6'
 print '[].any?        #==>: '
 p [].any?
 print '[].my_any?     #==>: '
 p [].my_any?
+puts ''
 
-puts '==============  my_map =============='
+puts '============== Point 6 - #my_none =============='
 
-puts 'my_map: Test 1'
-print '(1..4).map { |i| i * i }    #==>: '
-p (1..4).map { |i| i * i }
+puts 'my_none: Test 1'
+print '%w[ant bear cat].none?    { |word| word.length == 5 } #==>: '
+p %w[ant bear cat].none? { |word| word.length == 5 }
+puts ''
 
-print '(1..4).my_map { |i| i * i } #==>: '
-p (1..4).my_map { |i| i * i }
+print '%w[ant bear cat].my_none? { |word| word.length == 5 } #==>: '
+p %w[ant bear cat].my_none? { |word| word.length == 5 }
+puts ''
 
-puts 'my_map: Test 2'
-print '[4, 6, 7, 9].map { |x| x / 2 > 3  }    #==>: '
-p [4, 6, 7, 9].map { |x| x / 2 > 3 }
+puts 'my_none: Test 2'
+print '%w[ant bear cat].none?    { |word| word.length >= 4 } #==>: '
+p %w[ant bear cat].none? { |word| word.length >= 4 }
+print '%w[ant bear cat].my_none? { |word| word.length >= 4 } #==>: '
+p %w[ant bear cat].my_none? { |word| word.length >= 4 }
+puts ''
 
-print '[4, 6, 7, 9].my_map { |x| x / 2 > 3 } #==>: '
-p [4, 6, 7, 9].my_map { |x| x / 2 > 3 }
+puts 'my_none: Test 3'
+print '%w[ant bear cat].none?(/d/)     #==>: '
+p %w[ant bear cat].none?(/d/)
+print '%w[ant bear cat].my_none?(/d/)  #==>: '
+p %w[ant bear cat].my_none?(/d/)
+puts ''
 
-puts 'my_map: Test 3'
-print '[4, 6, 7, 9].map    #==>: '
-p [4, 6, 7, 9].map
+puts 'my_none: Test 4'
+print '[1, 3.14, 42].none?(Float)        #==>: '
+p [1, 3.14, 42].none?(Float)
+print '[1, 3.14, 42].my_none?(Float)     #==>: '
+p [1, 3.14, 42].my_none?(Float)
+puts ''
 
-print '[4, 6, 7, 9].my_map #==>: '
-p [4, 6, 7, 9].my_map
+puts 'my_none: Test 5'
+print '[].none?        #==>: '
+p [].none?
+print '[].my_none?     #==>: '
+p [].my_none?
+puts ''
 
-puts '==============  my_count =============='
+puts 'my_none: Test 6'
+print '[nil].none?        #==>: '
+p [nil].none?
+print '[nil].my_none?     #==>: '
+p [nil].my_none?
+puts ''
+
+puts 'my_none: Test 7'
+print '[nil,false].none?        #==>: '
+p [nil, false].none?
+print '[nil,false].my_none?     #==>: '
+p [nil, false].my_none?
+puts ''
+
+puts 'my_none: Test 8'
+print '[nil,false,true].none?        #==>: '
+p [nil, false, true].none?
+print '[nil,false,true].my_none?     #==>: '
+p [nil, false, true].my_none?
+puts ''
+
+puts '==============  Point 7 #my_count =============='
 puts 'my_any_full: Test 1'
 print '[1, 2, 3, 4].count    #==>: '
 p [1, 2, 3, 4].count
 
 print '[1, 2, 3, 4].my_count #==>: '
 p [1, 2, 3, 4].my_count
+puts ''
 
 puts 'my_any_full: Test 2'
 print '[1, 2, 3, 4].count(2)     #==>: '
@@ -433,20 +403,49 @@ p [1, 2, 3, 4].count(2)
 
 print '[1, 2, 3, 4].my_count(2)  #==>: '
 p [1, 2, 3, 4].my_count(2)
+puts ''
 
 puts 'my_any_full: Test 3'
 print '[1, 2, 3, 4].count { |x| (x % 2).zero }    #==>: '
 p [1, 2, 3, 4].count { |x| (x % 2).zero? }
 print '[1, 2, 3, 4].my_count { |x| (x % 2).zero } #==>: '
 p [1, 2, 3, 4].my_count { |x| (x % 2).zero? }
+puts ''
 
-puts '==============  my_inject =============='
+puts '==============  Point 8 #my_map =============='
+
+puts 'my_map: Test 1'
+print '(1..4).map { |i| i * i }    #==>: '
+p (1..4).map { |i| i * i }
+
+print '(1..4).my_map { |i| i * i } #==>: '
+p (1..4).my_map { |i| i * i }
+puts ''
+
+puts 'my_map: Test 2'
+print '[4, 6, 7, 9].map { |x| x / 2 > 3  }    #==>: '
+p [4, 6, 7, 9].map { |x| x / 2 > 3 }
+
+print '[4, 6, 7, 9].my_map { |x| x / 2 > 3 } #==>: '
+p [4, 6, 7, 9].my_map { |x| x / 2 > 3 }
+puts ''
+
+puts 'my_map: Test 3'
+print '[4, 6, 7, 9].map    #==>: '
+p [4, 6, 7, 9].map
+
+print '[4, 6, 7, 9].my_map #==>: '
+p [4, 6, 7, 9].my_map
+puts ''
+
+puts '==============  Point 9 #my_inject =============='
 puts 'my_any_full: Test 1'
 print 'p (5..10).inject { |sum, n| sum + n }    #==>: '
 p (5..10).inject { |sum, n| sum + n }
 
 print 'p (5..10).my_inject { |sum, n| sum + n } #==>: '
 p (5..10).my_inject { |sum, n| sum + n }
+puts ''
 
 puts 'my_any_full: Test 2'
 print 'p (5..10).inject(1) { |product, n| product * n }     #==>: '
@@ -454,17 +453,33 @@ p (5..10).inject(1) { |product, n| product * n }
 
 print 'p (5..10).my_inject(1) { |product, n| product * n }  #==>: '
 p (5..10).my_inject(1) { |product, n| product * n }
+puts ''
 
 puts 'my_any_full: Test 3'
 print 'p %w{ cat sheep bear }.inject { |memo, word| memo.length > word.length ? memo : word }     #==>: '
 p %w[cat sheep bear].inject { |memo, word| memo.length > word.length ? memo : word }
 print 'p %w{ cat sheep bear }.my_inject { |memo, word| memo.length > word.length ? memo : word }  #==>: '
 p %w[cat sheep bear].my_inject { |memo, word| memo.length > word.length ? memo : word }
+puts ''
 
-puts '==============  POINT 10 - multiply_else =============='
+puts '==============  POINT 10 - #my_inject test - multiply_else =============='
 puts 'mutilply_els using my_inject: Test 1'
 print 'p multiply_els([2, 4, 5])  #==>: '
 p multiply_els([2, 4, 5])
+puts ''
+
+puts '==============  POINT 11/12 - #my_map modified =============='
+
+my_proc = proc { |arg1| arg1 / 2 > 3 }
+
+puts 'my_map: Test using normal block'
+print '[4, 6, 7.0, 9].my_map { |x| x / 2 > 3  }    #==>: '
+p [4, 6, 7.0, 9].my_map { |x| x / 2.0 > 3.0 }
+puts ''
+
+puts 'my_map: Test using proc & block - Executing Proc as priority'
+print '[4, 6, 7.0, 9].my_map_proc(my_proc) {|arg1| arg1 / 2.0 > 3.0 } #==>: '
+p [4, 6, 7.0, 9].my_map(my_proc) { |arg1| arg1 / 2.0 > 3.0 }
 
 # rubocop:enable Lint/AmbiguousBlockAssociation
 # rubocop:enable Lint/ParenthesesAsGroupedExpression
